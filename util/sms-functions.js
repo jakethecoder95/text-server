c30adb549dc054b9161bb6d40cfcbc8b93c8c81d
Jacob Robinson <95jacob07@gmail.com>
2019-09-23 16:30:17 -0700

    more descriptive var"

--- util/sms-functions.js
+++ util/sms-functions.js
@@ -3,10 +3,10 @@ if (process.env.NODE_ENV !== "production") require("dotenv").config();
 const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
 const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
 
-const client = require("twilio")(twilioAccountSid, twilioAuthToken);
+const twilio = require("twilio")(twilioAccountSid, twilioAuthToken);
 
 exports.sendSms = async (from, to, msg) =>
-  await client.messages.create({
+  await twilio.messages.create({
     to,
     from,
     body: msg
