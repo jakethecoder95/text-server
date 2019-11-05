f6281ba8f48e8d1e991c54ebbfe083d6e42e0ee8
Jacob Robinson <95jacob07@gmail.com>
2019-11-04 19:49:47 -0800

    Not useing await to send each message

--- util/sms-functions.js
+++ util/sms-functions.js
@@ -5,8 +5,8 @@ const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
 
 const twilio = require("twilio")(twilioAccountSid, twilioAuthToken);
 
-exports.sendSms = async (from, to, msg) =>
-  await twilio.messages.create({
+exports.sendSms = (from, to, msg) =>
+  twilio.messages.create({
     to,
     from,
     body: msg
