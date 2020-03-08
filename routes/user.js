c5c10e4be482984d4bfb3876e757144a2eb92205
Jacob Robinson <95jacob07@gmail.com>
2020-03-07 17:26:46 -0800

    Clearnup unneccessary files

--- routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-
-const isAuth = require("../middleware/is-auth");
-const userController = require("../controllers/user");
-
-const router = express.Router();
-
-module.exports = router;
